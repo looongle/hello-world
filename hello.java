058d0aa0cabbdc2a5784ec053f39575422b17623
looongle <89523722+looongle@users.noreply.github.com>
2021-08-25 08:07:54 -0400

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,12 @@
+  public class hello {
+	public static void main(String[] args) { 
+		
+		System.out.println("<Long Le>");
+		System.out.println("<Java 1.8>");
+		System.out.println("<Sublime>");
+		System.out.println("<8/24/2021>");
+		System.out.println("<CMSC255 and CRN 37573>");
+		System.out.println();
+	}
+}
+
